4579c263dd7c8c8355ef537762dab500af296f09
DiasOrisbek <orisbekdias@gmail.com>
2022-09-12 12:29:40 +0600

    First commit Message

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module diasorisbek.hellolesson1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens diasorisbek.hellolesson1 to javafx.fxml;
+    exports diasorisbek.hellolesson1;
+}
